20b1b1e9c38156502889c95a6974b43c7f525cfe
KKG78 <kishan560kumar@gmail.com>
2024-05-21 09:25:47 +0530

    Updated settings.py

--- django-project/taskmate/taskmate/settings.py
+++ django-project/taskmate/taskmate/settings.py
@@ -25,7 +25,6 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1", "taskmate.up.railway.app"]
 CSRF_TRUSTED_ORIGINS = ['https://taskmate.up.railway.app']
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
