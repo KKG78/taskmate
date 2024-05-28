7ac738aef9cdc599417510214e6a9ac5c8b09005
KKG78 <kishan560kumar@gmail.com>
2024-05-28 20:13:08 +0530

    Settings Updated

--- django-project/taskmate/taskmate/settings.py
+++ django-project/taskmate/taskmate/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY=env.str("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "taskmate.up.railway.app"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "taskmate-yip2.onrender.com"]
 CSRF_TRUSTED_ORIGINS = ['https://taskmate.up.railway.app']
 
 # Application definition
